8ea593f125ab893094233a6941f254f2a8bfbf43
oussema-bit <80320778+oussema-bit@users.noreply.github.com>
2021-10-24 08:53:40 +0100

    Delete Desktop/GOMYCODE/DOM directory

--- Desktop/GOMYCODE/DOM/CSS/logic.js
+++
@@ -1,9 +0,0 @@
-function validateForm(e) {
-    e.preventDefault();
-    var name = document.loginForm.name.value;
-    var password = document.loginForm.password.value;
-   if (name.length==0)
-    return alert(`name is required`);
-   if (password.length<5)
-    return alert(`password length should more than 5`);
-   }
