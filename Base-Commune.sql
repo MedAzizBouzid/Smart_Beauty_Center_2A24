57c8e792894d3f850fe72c7dd6bc6e49bdc918c8
mariiiiiiiem <mariem.belhajabdallah@esprit.tn>
2021-11-01 11:05:40 +0100

    committ

---
+++ Base-Commune.sql
@@ -0,0 +1,91 @@
+DROP TABLE Choisir;
+DROP TABLE Service;
+DROP TABLE Comptabilite;
+DROP TABLE Servir;
+DROP TABLE Client;
+DROP TABLE Manipuler;
+DROP TABLE Employe;
+DROP TABLE Fournir;
+DROP TABLE Produits; --drop table supprime l'ancien table et mise a jour des nouveaux données 
+DROP TABLE Fournisseurs;
+CREATE TABLE Fournisseurs(
+Code_F NUMBER ,
+Localisation VARCHAR2(50 char) ,
+Num_tel NUMBER UNIQUE,
+Type_P VARCHAR2(50 char),
+Prix FLOAT,
+CONSTRAINT Fournisseurs_pk PRIMARY KEY(Code_F)
+);
+CREATE TABLE Produits(
+Code_abar NUMBER ,
+Nom VARCHAR2(50 char) ,
+Type_P VARCHAR2(50 char) ,
+Quantite int,
+Prix FLOAT,
+CONSTRAINT Produits_pk PRIMARY KEY(Code_abar)
+);
+CREATE TABLE Fournir(
+Produit NUMBER,
+Fournisseur NUMBER,
+CONSTRAINT Fournir_Produit_fk Foreign KEY(Produit) REFERENCES Produits(Code_abar),
+CONSTRAINT Fournir_Fournisseur_fk Foreign KEY(Fournisseur) REFERENCES Fournisseurs(Code_F),
+CONSTRAINT Fournir_Produit_Fournisseur_pk Primary KEY(Fournisseur, produit) 
+);
+CREATE TABLE Employe(
+CIN NUMBER,
+Num_tel NUMBER UNIQUE,
+Nom VARCHAR2 (50 char),
+Prenom VARCHAR2 (50 char),
+Adresse VARCHAR2(50 char),
+Role VARCHAR2(50 char),
+CONSTRAINT Employe_pk PRIMARY KEY(CIN)
+);
+CREATE TABLE Manipuler(
+Prod NUMBER,
+Emp NUMBER,
+CONSTRAINT Manipuler_Produit_fk Foreign KEY(Prod) REFERENCES Produits(Code_abar),
+CONSTRAINT Manipuler_Employe_fk Foreign KEY(Emp) REFERENCES Employe(CIN),
+CONSTRAINT Manipuler_Employe_Produit_pk Primary KEY(Prod, Emp)
+);
+CREATE TABLE Client(
+Code_C NUMBER,
+Num_tel NUMBER UNIQUE,
+Nom VARCHAR2 (50 char),
+Prenom VARCHAR2 (50 char),
+Adresse_Mail VARCHAR2(50 char),
+Date_RDV DATE,
+S_Attribue NUMBER,
+CONSTRAINT Client_pk PRIMARY KEY(Code_C)
+);
+CREATE TABLE Servir(
+Cli NUMBER,
+Emp NUMBER,
+CONSTRAINT Servir_Client_fk Foreign KEY(Cli) REFERENCES Client(Code_C),
+CONSTRAINT Servir_Employe_fk Foreign KEY(Emp) REFERENCES Employe(CIN),
+CONSTRAINT Servir_Employe_Client_pk Primary KEY(Emp,Cli)
+);
+CREATE TABLE Comptabilite(
+Code_Fact NUMBER,
+Categorie VARCHAR2(50 char),
+Date_Paiement DATE,
+Somme FLOAT,
+Descriptions VARCHAR2(50 char),
+CIN_Tresorier NUMBER,
+CONSTRAINT Comptabilite_pk PRIMARY KEY(Code_Fact),
+CONSTRAINT Comptabilite_Employe_fk Foreign KEY(CIN_Tresorier) REFERENCES Employe(CIN)
+);
+CREATE TABLE Service(
+Code_S NUMBER,
+Type_S VARCHAR2(50 char),
+Descriptions VARCHAR2(50 char),
+Prix FLOAT,
+CONSTRAINT Service_pk PRIMARY KEY(Code_S)
+);
+CREATE TABLE Choisir(
+Serv NUMBER,
+Cli NUMBER,
+CONSTRAINT Choisir_Client_fk Foreign KEY(Cli) REFERENCES Client(Code_C),
+CONSTRAINT Choisir_Service_fk Foreign KEY(Serv) REFERENCES Service(Code_S),
+CONSTRAINT Choisir_Service_Client_pk Primary KEY(Serv,Cli) 
+
+);
